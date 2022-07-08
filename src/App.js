9e71e5abd09d8a06a6516b4a142a4be23de139b5
Natalia <linata81@gmail.com>
2022-07-08 21:04:36 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -9,16 +9,13 @@ function App() {
   const [foundPairs, setFoundPairs] = useState([]);
   const [clicks, setClicks] = useState(0);
   const [start, setStart] = useState(false);
+  const [timerOn, setTimerOn] = useState(false);
   const [time, setTime] = useState({ minute: 0, second: 0 });
   const [won, setWon] = useState(false);
   
-  // let interval = null;
-
-  
   useEffect (() => {
     let interval = null;
-    
-    if(start) {
+    if(timerOn) {
       interval = setInterval(() => {
         setTime(prev => {
           return {
@@ -27,30 +24,27 @@ function App() {
           }
         })
       }, 1000);
-      
-      return () => {
-        clearInterval(interval);
-      }
+    } else {
+      clearInterval(interval);
     }
+    return () => clearInterval(interval);
     
-  },[start]);
+  },[timerOn]);
   
   useEffect(() => {
-    // if(won) {
-    //   clearInterval(interval);
-    // }
-  }, [won])
+    if(won) {
+      setTimerOn(false);
+    }
+  },[won])
+  
+  const runStart = () => {
+    setStart(true);
+    setTimerOn(true);
+  }
   
   function flipCard(index,e) {
-    if(won) {
-      setCards(shuffle([...Images, ...Images]));
-      setActiveCards([]);
-      setFoundPairs([]);
-      setClicks(0);
-      setStart(false);
-    }
     
-    if(start) {    
+    if(start && !won) {    
       if(activeCards.length === 0) {
         setActiveCards([index]);
       }
@@ -62,7 +56,6 @@ function App() {
 
           if(foundPairs.length + 2 === cards.length) {
             setWon(true);
-            console.log('fired');
           }
           
           setFoundPairs([...foundPairs, firstIndex, secondIndex])
@@ -84,6 +77,7 @@ function App() {
     setFoundPairs([]);
     setClicks(0);
     setStart(false);
+    setWon(false);
     setTime({ minute: 0, second: 0 });
   }
   
@@ -107,10 +101,15 @@ function App() {
       <div className={'stats' + (start ? " visible" : '')}>
         clicks: {clicks} &nbsp;&nbsp; Found pairs: {foundPairs.length / 2}
       </div>
-      <button className={"btn" + (!start ? " visible" : '')} onClick={() => setStart(true)}>Start</button>
+      
+      {won && (<div>You won the game! Congratulations!<br/></div>)}
+      
+      <button className={"btn" + (!start ? " visible" : '')} onClick={runStart}>Start</button>
+      
       <div className={'icon' + (start ? " visible" : '')} onClick={reloadHandler}>
         <svg viewBox="0 0 119.4 122.88"><path d="M83.91,26.34a43.78,43.78,0,0,0-22.68-7,42,42,0,0,0-24.42,7,49.94,49.94,0,0,0-7.46,6.09,42.07,42.07,0,0,0-5.47,54.1A49,49,0,0,0,30,94a41.83,41.83,0,0,0,18.6,10.9,42.77,42.77,0,0,0,21.77.13,47.18,47.18,0,0,0,19.2-9.62,38,38,0,0,0,11.14-16,36.8,36.8,0,0,0,1.64-6.18,38.36,38.36,0,0,0,.61-6.69,8.24,8.24,0,1,1,16.47,0,55.24,55.24,0,0,1-.8,9.53A54.77,54.77,0,0,1,100.26,108a63.62,63.62,0,0,1-25.92,13.1,59.09,59.09,0,0,1-30.1-.25,58.45,58.45,0,0,1-26-15.17,65.94,65.94,0,0,1-8.1-9.86,58.56,58.56,0,0,1,7.54-75,65.68,65.68,0,0,1,9.92-8.09A58.38,58.38,0,0,1,61.55,2.88,60.51,60.51,0,0,1,94.05,13.3l-.47-4.11A8.25,8.25,0,1,1,110,7.32l2.64,22.77h0a8.24,8.24,0,0,1-6.73,9L82.53,43.31a8.23,8.23,0,1,1-2.9-16.21l4.28-.76Z"/></svg>
       </div>
+      
       <div className={'timer' + (start ? " visible" : '')}>
         <div>{time.minute <= 9 ? `0${time.minute}`: time.minute}</div> 
         <span>:</span>
